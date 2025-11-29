56fa5e79ced89dfd8b0926c66aa1b9922217d03b
Dominic Sisneros <dominic@Mac.lan>
2025-11-29 01:05:26 -0700

    multipane dashboard

---
+++ examples/test_terminal.cr
@@ -0,0 +1,24 @@
+require "../src/term2"
+
+class TestModel < Term2::Model
+  def init : Term2::Cmd
+    Term2::Cmd.none
+  end
+
+  def update(msg : Term2::Message) : {Term2::Model, Term2::Cmd}
+    case msg
+    when Term2::WindowSizeMsg
+      puts "Window size: #{msg.width}x#{msg.height}"
+    end
+    {self, Term2::Cmd.none}
+  end
+
+  def view : String
+    "Hello from Term2!\n"
+  end
+end
+
+if PROGRAM_NAME.includes?(__FILE__)
+  model = TestModel.new
+  Term2.run(model)
+end
